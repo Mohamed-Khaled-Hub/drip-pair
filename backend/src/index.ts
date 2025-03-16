2216414674f4268d167030245c013d330185d483
Khaled <mohameddkhaled980@gmail.com>
2025-03-16 12:15:07 +0200

    Deployment edit for backend

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -18,6 +18,8 @@ app.use(
     cors({
         origin: [`${process.env.FRONTEND_URL}`],
         methods: ['GET', 'POST', 'PUT', 'DELETE'],
+        allowedHeaders: ['Content-Type', 'Authorization'],
+        credentials: true,
     })
 )
 
