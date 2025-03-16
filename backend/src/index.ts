d54265cf7850c043526f8bd6aeca3ac89f81ce9f
Khaled <mohameddkhaled980@gmail.com>
2025-03-16 12:18:59 +0200

    Deployment edit for backend

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -15,12 +15,14 @@ const app = express()
 
 app.use(express.json())
 app.use(
-    cors({
-        origin: [`${process.env.FRONTEND_URL}`],
-        methods: ['GET', 'POST', 'PUT', 'DELETE'],
-        allowedHeaders: ['Content-Type', 'Authorization'],
-        credentials: true,
-    })
+    cors(
+        // {
+        //     origin: [`${process.env.FRONTEND_URL}`],
+        //     methods: ['GET', 'POST', 'PUT', 'DELETE'],
+        //     allowedHeaders: ['Content-Type', 'Authorization'],
+        //     credentials: true,
+        // }
+    )
 )
 
 app.use(logger)
