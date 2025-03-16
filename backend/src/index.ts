147e9c1a0de06b697d851b291a8f32fe30244296
Khaled <mohameddkhaled980@gmail.com>
2025-03-16 12:11:38 +0200

    Deployment edit for backend

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -16,7 +16,7 @@ const app = express()
 app.use(express.json())
 app.use(
     cors({
-        origin: [`${process.env.FRONTEND_URL}:${process.env.FRONTEND_PORT}`],
+        origin: [`${process.env.FRONTEND_URL}`],
         methods: ['GET', 'POST', 'PUT', 'DELETE'],
     })
 )
